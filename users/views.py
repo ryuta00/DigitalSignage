85f1cd30a90db9531c0a31bedabb710f30c369d1
ryuta00 <s1f101900936@iniad.org>
2022-11-28 15:43:49 +0900

    アジア圏の人の顔を400件登録

--- users/views.py
+++ users/views.py
@@ -53,7 +53,7 @@ def cos_similarity(p1, p2):
 def create_student(image_path):
     mtcnn = MTCNN(image_size=160, margin=10)
     resnet = InceptionResnetV1(pretrained='vggface2').eval()
-    img = Image.open(settings.STATIC_ROOT + "/images/" + image_path)
+    img = Image.open(settings.STATIC_ROOT + "/images/dataset/" + image_path)
     img_cropped = mtcnn(img)
     img_embedding = resnet(img_cropped.unsqueeze(0))
     p = img_embedding.squeeze().to('cpu').detach().numpy().copy()
@@ -348,51 +348,18 @@ def start(request):
     # 1回目の実行では学習済みのモデルをダウンロードしますので、少し時間かかります。
     resnet = InceptionResnetV1(pretrained='vggface2').eval()
 
-    # 三人分の比較をします。
-    # 1つ目をカメラで取得した人として
-    # 2、3つ目を登録されている人とします。
     image_path = settings.STATIC_ROOT + "/images/face.jpg"
-    image_path1 = settings.STATIC_ROOT + "/images/face.jpg"
-    #image_path2 = settings.STATIC_ROOT + "/images/face2.jpg"
-    #image_path3 = settings.STATIC_ROOT + "/images/face3.jpg"
 
     img0 = Image.open(image_path)
     img_cropped0 = mtcnn(img0)
     img_embedding0 = resnet(img_cropped0.unsqueeze(0))
 
-    # (仮)カメラで取得した方
-    # 画像データ取得
-    img1 = Image.open(image_path1)
-    # 顔データを160×160に切り抜き
-    img_cropped1 = mtcnn(img1)
-    # save_pathを指定すると、切り取った顔画像が確認できます。
-    # img_cropped1 = mtcnn(img1, save_path="cropped_img1.jpg")
-    # 切り抜いた顔データを512個の数字に
-    img_embedding1 = resnet(img_cropped1.unsqueeze(0))
-
-    # (仮)登録されたカメラと同じ人
-    #img2 = Image.open(image_path2)
-    #img_cropped2 = mtcnn(img2)
-    #img_embedding2 = resnet(img_cropped2.unsqueeze(0))
-
-    # (仮)登録されたカメラと違う人
-    #img3 = Image.open(image_path3)
-    #img_cropped3 = mtcnn(img3)
-    #img_embedding3 = resnet(img_cropped3.unsqueeze(0))
 
     # 512個の数字にしたものはpytorchのtensorという型なので、numpyの方に変換
     p0 = img_embedding0.squeeze().to('cpu').detach().numpy().copy()
-    p1 = img_embedding1.squeeze().to('cpu').detach().numpy().copy()
-    #p2 = img_embedding2.squeeze().to('cpu').detach().numpy().copy()
-    #p3 = img_embedding3.squeeze().to('cpu').detach().numpy().copy()
-
-    # 類似度を計算して顔認証
-    #img1vs2 = cos_similarity(p1, p2)
-    #img1vs3 = cos_similarity(p1, p3)
-
     # テストデータ登録
     for i in range(0, 400):
-        name = "0" * (6 - len(str(i))) + str(i) + ".jpg"
+        name = str(i) + ".jpg"
         create_student(name)
 
     # ユーザー登録
@@ -400,11 +367,6 @@ def start(request):
         name="野口龍太A",
         mail="s1f101900936@iniad.org",
         vector_path=save_vector(p0)
-    )
-    Student.objects.create(
-        name="野口龍太B",
-        mail="s1f101900936@iniad.org",
-        vector_path=save_vector(p1)
     )
 
     News.objects.create(
@@ -417,4 +379,4 @@ def start(request):
         text="自動火災報知器の定期点検を12月23日13時より実施いたします。当日ご都合の悪い方は事前にお知らせください。"
     )
 
-    return HttpResponse(p1)
+    return HttpResponse(p0)
